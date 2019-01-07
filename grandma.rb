78ea29d45d46e6992f9d3f41407ff457d38b7dcc
Rafael Vargas <vargas718@gmail.com>
2019-01-07 02:24:04 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,12 +1,3 @@
-def speak_to_grandma("phrase")
-  if phrase != "phrase".upcase
-    puts "HUH?! SPEAK UP, SONNY!"
-  else phrase == "phrase".upcase
-    puts "NO, NOT SINCE 1938!"
-    elsif phrase = "I LOVE YOU GRANDMA!"
-      puts "I LOVE YOU TOO PUMPKIN"
-    end
-  end
 
 # Whatever you say to grandma, she should respond with
 # HUH?! SPEAK UP, SONNY!
